d167d8607f0885f2f2f87d4f9f642820f521f67e
Anastasia Mistreanu <anastasiateodora04@gmail.com>
2026-08-09 23:33:50 +0300

    Add target drugs reference list

---
+++ src/drugs.py
@@ -0,0 +1,19 @@
+#Drugs used in the FDA Adverse Event Detector project
+
+TARGET_DRUGS = [
+    "Ibuprofen",
+    "Aspirin",
+    "Acetaminophen",
+    "Metformin",
+    "Insulin glargine",
+    "Atorvastatin",
+    "Lisinopril",
+    "Warfarin",
+    "Apixaban",
+    "Sertraline",
+    "Alprazolam",
+    "Oxycodone",
+    "Amoxicillin",
+    "Gabapentin",
+    "Levothyroxine",
+]
